c98aee797539cf4c17092b2e7bf1f59fa13c1870
vladyslavJs <vladyslavmelnyk15@gmail.com>
2024-03-22 00:04:38 +0100

    update-App

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,16 @@
+import ContactForm from '../ContactForm/ContactForm'
+import SearchBox from '../SearchBox/SearchBox'
+import ContactList from '../ContactList/ContactList'
+
+import css from './App.module.css'
+
 export default function App() {
   return (
-    <>
-      <div>Redux</div>
-    </>
+    <div className={css.container}>
+      <h1 className={css.title}>Contacts</h1>
+      <SearchBox />
+      <ContactForm />
+      <ContactList/>
+    </div>
   )
 }
